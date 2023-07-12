88c1da6ba16c0d37218a3e0120a90bce6900bd41
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-07-12 13:41:01 +0200

    added function uptotop to button

--- src/js/uptotop.js
+++ src/js/uptotop.js
@@ -5,11 +5,11 @@ function scrollToTop(){
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
-function toggleTopButton() {
-  if (document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20) {
-    document.getElementById('back-to-up').classList.remove('d-none');
-  } else {
-    document.getElementById('back-to-up').classList.add('d-none');
-  }
-}
+// function toggleTopButton() {
+//   if (document.body.scrollTop > 50 ||
+//       document.documentElement.scrollTop > 50) {
+//     document.getElementById('back-to-up').classList.remove('visually-hidden');
+//   } else {
+//     document.getElementById('back-to-up').classList.add('visually-hidden');
+//   }
+// }
