b47664c98f20da503ec77d09b85d31b1f6649c63
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-07-14 23:22:43 +0200

    test

--- src/js/uptotop.js
+++ src/js/uptotop.js
@@ -1,10 +1,10 @@
 window.onscroll = () => {
   toggleTopButton();
-}
+};
 
 function scrollToTop(){
   window.scrollTo({top: 0, behavior: 'smooth'});
-}
+};
 
 function toggleTopButton() {
   if (document.body.scrollTop > 50 ||
@@ -13,26 +13,25 @@ function toggleTopButton() {
   } else {
     document.getElementById('back-to-up').classList.add('visually-hidden');
   }
-}
+};
 
 function setSvgWidth() {
-  var svg = document.getElementById('uptotop-svg');
-  var screenWidth = window.innerWidth;
+   var svg = document.getElementById('uptotop-svg');
+   var screenWidth = window.innerWidth;
 
 
-  
-  // Set the width based on conditions
-  if (screenWidth >= 768) {
-    svg.setAttribute('width', '16');
-    svg.setAttribute('height', '30');
-  } else {
-    svg.setAttribute('width', '8');
-    svg.setAttribute('height', '16');
-  }
-}
+     // Set the width based on conditions
+    if (screenWidth >= 768) {
+     svg.setAttribute('width', '16');
+     svg.setAttribute('height', '30');
+   } else {
+     svg.setAttribute('width', '8');
+     svg.setAttribute('height', '16');
+   }
+};
 
 window.addEventListener('resize', function() {
-  setSvgWidth();
+   setSvgWidth();
 });
 
 // Initialize the width of the SVG image
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './js/mobile-menu';
 import './js/pop-up';
 import './js/slider';
-import './js/upToTop';
-import './js/swiper-bundle.min';
+import './js/uptotop';
