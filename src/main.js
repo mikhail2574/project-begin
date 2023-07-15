86e970bf9486155bff1fb21f935caf6e69cd9c80
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2023-07-15 09:54:59 +0200

    comment modal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-// import './js/uptotop';
+import './js/uptotop';
 import './js/mobile-menu'
-import './js/modal'
-// import './js/mobile-menu';
+// import './js/modal'
 import './js/pop-up';
 import './js/slider';
